9944014cbca2804a3b665f0a5341ce7c09da559f
zhouhao <zhou_hao@mutongtech.com>
2019-03-26 06:53:41 +0800

    启动Spring Boot

---
+++ springboot/src/main/java/com/zhouhao/study/springboot/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.zhouhao.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootApplication {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello Spring boot";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+}
